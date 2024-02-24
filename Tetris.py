d93ec859cf151a464aa112fb0f282692c1755ab7
Henriddiaz <henryddiaz696@gmail.com>
2024-02-23 22:59:22 -0600

    added animations

--- Tetris.py
+++ Tetris.py
@@ -1,4 +1,5 @@
 import pygame
+from copy import deepcopy
 
 W, H = 10, 20
 TILE = 34
@@ -22,22 +23,53 @@ figure_pos = [[(-1, 0),(-2, 0),(0, 0),(1, 0)],
 figures = [[pygame.Rect(x +W // 2, y + 1, 1, 1)for x, y in fig_pos]for fig_pos in figure_pos]
 figure_rect = pygame.Rect(0, 0, TILE -2, TILE -2)
 
-figure = figures[0]
+anim_count, anim_speed, anim_limit = 0, 60, 20000
+figure = deepcopy(figures[3])
+
+def check_borders():
+    if figure[i].x < 0 or figure[i].x > W - 1:
+        return False
+    return True
 
 while True:
-        game_sc.fill(pygame.Color('black'))
-        
-        for event in pygame.event.get():
+    dx = 0
+    game_sc.fill(pygame.Color('black'))
+    #control
+    for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 exit()
-                
-        # draw grid
-        [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1)for i_rect in grid]                
-        # draw figure
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_LEFT:
+                    dx = -1
+                elif event.key == pygame.K_RIGHT:
+                    dx = 1
+                elif event.key == pygame.K_DOWN:
+                    anim_limit = 1000
+    # move x
+    figure_old = deepcopy(figure)
+    for i in range(4):
+        figure[i].x += dx
+        if not check_borders():
+            figure = deepcopy(figure_old)
+            break
+    # move y
+    anim_count += anim_speed
+    if anim_count > anim_limit:
+        anim_count = 0
+        figure_old = deepcopy(figure)
         for i in range(4):
-            figure_rect.x = figure[i].x * TILE 
-            figure_rect.y = figure[i].y * TILE
-            pygame.draw.rect(game_sc, pygame.Color('white'), figure_rect)
+            figure[i].y += 1
+            if not check_borders():
+                figure = deepcopy(figure_old)
+                anim_limit = 2000
+                break
+    # draw grid
+    [pygame.draw.rect(game_sc, (40, 40, 40), i_rect, 1)for i_rect in grid]                
+    # draw figure
+    for i in range(4):
+        figure_rect.x = figure[i].x * TILE 
+        figure_rect.y = figure[i].y * TILE
+        pygame.draw.rect(game_sc, pygame.Color('white'), figure_rect)
         
-        pygame.display.flip()
-        clock.tick()
+    pygame.display.flip()
+    clock.tick()
